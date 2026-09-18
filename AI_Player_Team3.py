6b82e5968805528712b39fa49d2e4de990f21718
bjorn <b.stephany@student.maastrichtuniversity.nl>
2026-09-18 15:45:24 +0200

    make get_legal_moves into helper

--- AI_Player_Team3.py
+++ AI_Player_Team3.py
@@ -23,34 +23,38 @@ def AI_Player_Team3(
 # CHECK IF BOARD IS 5x5
     if len(board) != 5 or any(len(row) != 5 for row in board):
             raise ValueError("Board must be 5 by 5")
-    
-# INITIALIZE EMPTY LIST FOR LEGAL MOVES
-    legal_moves: List[Tuple[Tuple[int, int], Tuple[int, int]]] = []
-    
-# SEARCH ENTIRE BOARD ADDING ALL LEGAL MOVES
-    for row in range(5):
-            for column in range(5):
-                if board[row][column] != player:
-                    continue
-    
-                oldPos: Tuple[int, int] = (row, column)
-    
-                for new_row in range(5):
-                    for new_column in range(5):
-                        newPos: Tuple[int, int] = (new_row, new_column)
-    
-                        if check_legal_move(board, oldPos, newPos):
-                            legal_moves.append((oldPos, newPos))
-                            
-    if not legal_moves:
-            raise ValueError(f"Player {player} has no legal moves")
         
-    # SOME LOGIC HERE TO DO:
-    
-    
+    # SOME LOGIC HERE TO DO
     
     # CONVERT COORDINATES TO BOARD NOTATION
     old_reference: str = chr(ord("A") + oldPos[1]) + str(oldPos[0] + 1)
     new_reference: str = chr(ord("A") + newPos[1]) + str(newPos[0] + 1)
     
     return old_reference, new_reference
+
+
+def get_legal_moves(
+        board: List[List[int]],
+        player: int
+) -> List[Tuple[Tuple[int, int], Tuple[int, int]]]:
+        
+        # INITIALIZE EMPTY LIST FOR LEGAL MOVES
+        legal_moves: List[Tuple[Tuple[int, int], Tuple[int, int]]] = []
+        
+        # SEARCH ENTIRE BOARD ADDING ALL LEGAL MOVES
+        for row in range(5):
+                for column in range(5):
+                        if board[row][column] != player:
+                                continue
+        
+                        oldPos: Tuple[int, int] = (row, column)
+        
+                        for new_row in range(5):
+                                for new_column in range(5):
+                                        newPos: Tuple[int, int] = (new_row, new_column)
+                
+                                        if check_legal_move(board, oldPos, newPos):
+                                                legal_moves.append((oldPos, newPos))
+                                
+        if not legal_moves:
+                raise ValueError(f"Player {player} has no legal moves")
