383ace45c39f1aca4ca1f8b22b09df4ae4a964cc
aaryan2987 <aaryan2987@gmail.com>
2026-09-22 15:29:27 +0200

    simpified code and removes unnecessary variables/functions

--- AI_Player_Team3.py
+++ AI_Player_Team3.py
@@ -112,10 +112,6 @@ def order_moves(moves, player):
 
     return [move for gain, move in ordered_moves]
 
-
-def format_scores(scores) -> str:
-    return "(" + ", ".join(f"{score:.1f}" for score in scores) + ")"
-
 # Max^n search function with shallow pruning
 def maxn(
     board,
@@ -138,10 +134,6 @@ def maxn(
     )
 
     if key in seen:
-
-        if parent_id is not None:
-            tree.get_node(parent_id).tag += " [repeat]"
-
         return seen[key], None
 
     moves = get_legal_moves(board, player)
@@ -164,7 +156,7 @@ def maxn(
     best_move = None
     was_pruned = False
 
-    for index, move in enumerate(moves):
+    for move in moves:
 
         old_position, new_position = move
 
@@ -213,9 +205,6 @@ def maxn(
         board[new_row][new_column] = 0
         board[old_row][old_column] = player
 
-        if child_id is not None:
-            tree.get_node(child_id).tag += " -> " + format_scores(scores)
-
         if (
             best_scores is None
             or scores[player - 1] > best_scores[player - 1]
@@ -226,17 +215,7 @@ def maxn(
         maximum_possible = SCORE_TOTAL - parent_best
 
         if best_scores[player - 1] >= maximum_possible - 1e-9:
-
             was_pruned = True
-
-            if parent_id is not None:
-                skipped = len(moves) - index - 1
-
-                if skipped > 0:
-                    tree.get_node(parent_id).tag += (
-                        f" [pruned {skipped} moves]"
-                    )
-
             break
 
     if not was_pruned:
